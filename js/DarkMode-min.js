94fe4725b7d27b473e9368661b841ef11dd988d5
Rafał Mucha <rmucha@cisco.com>
2023-12-20 14:11:17 +0100

    Class names cleanup

--- js/DarkMode-min.js
+++ js/DarkMode-min.js
@@ -1,1 +1,104 @@
-function addSrc(e){document.getElementById(e).src="https://"+e}function addSrcImg(e){document.getElementById(e).src=e}function playSaber(e){var a=new Audio(e);a.play(),a.volume=.3,localStorage.setItem("dark","true")}-1!=navigator.userAgent.indexOf("Chrome")||-1!=navigator.userAgent.indexOf("Edg")||navigator.userAgent.indexOf("Firefox"),"true"===sessionStorage.getItem("dismiss")&&(document.getElementById("infoAlert").style.display="none",$("#infoAlert").alert("close")),window.onload=function(){document.getElementById("loading-container").style.display="none",$(this).scrollTop()>9e3&&$(".img-profile").attr("src","img/FACE2.png"),$(this).scrollTop()<9e3&&$(".img-profile").attr("src","img/profile.png")},$((function(){function e(){$("html").addClass("dark_mode_on"),$("#sideNav").removeClass("bg-primary").addClass("bg-dark"),$("#darkMode").hide(),$("#lightMode").show(),$("body").addClass("darkness"),$("#infoAlert").removeClass("inset").addClass("sabre"),$(".list-group-flush .list-group-item").addClass("darkness3"),$("#skillsAccordion").css("background-color","#222"),$("li .btn").removeClass("btn-light").addClass("btn-dark"),$(".badge-dark").addClass("badge-light").removeClass("badge-dark"),$("h1,h2,h3").addClass("darkness2"),$(".badge-info").addClass("badge-danger").removeClass("badge-info"),$(".text-primary").addClass("text-danger").removeClass("text-primary"),$(".flipLink").addClass("hovCol"),$("#HeaderName").css("color","#ddd"),$(".fa-ul a, .subheading a").css("color","#c20404"),$(".bg-info").addClass("bg-danger").removeClass("bg-info"),$("#actionBtn").css("background-color","#c20404"),$("h4 .text-info").addClass("text-danger").removeClass("text-info"),$("h4 .text-form").addClass("text-form-dark").removeClass("text-form"),$(".blueChange").addClass("redChange").removeClass("blueChange"),$("#GSAP").addClass("redChange").removeClass("blueChangeGSAP"),$("i.devicons, i.newIcon, .fa-stack.fa-lg").addClass("hovCol"),$(".img-profile").addClass("Sith").removeClass("Jedi"),$("#actionBtnBug").css("background-color","#ba8b00"),$("#actionBtnChat").css("background-color","#fd7e14"),$(".modalAddon").addClass("invertedBtn"),$(".modImg").addClass("modImgSith"),$("#loading-wrapper").addClass("inverted"),$(".secHeading").addClass("subDark"),$(".chatSend").removeClass("btn-outline-info").addClass("btn-outline-danger"),$(".chatHead, .chatHolder").css("background-color","#343a40"),$(".chatInput").css("background-color","#111")}$(window).scroll((function(){$(this).scrollTop()>9e3&&$(".img-profile").attr("src","img/FACE2.png"),$(this).scrollTop()<9e3&&$(".img-profile").attr("src","img/profile.png")})),document.getElementById("darkMode").addEventListener("click",e),$("#lightMode").on("click",(function(){$("html").removeClass("dark_mode_on"),$("#sideNav").removeClass("bg-dark").addClass("bg-primary"),$("#lightMode").hide(),$("#darkMode").show(),$("body").removeClass("darkness"),$("#infoAlert").removeClass("sabre").addClass("inset"),$(".list-group-flush .list-group-item").removeClass("darkness3"),$("#skillsAccordion").css("background-color","#fff"),$("li .btn").removeClass("btn-dark").addClass("btn-light"),$(".badge-light").addClass("badge-dark").removeClass("badge-light"),$("h1,h2,h3").removeClass("darkness2"),$(".badge-danger").addClass("badge-info").removeClass("badge-danger"),$(".text-danger").addClass("text-primary").removeClass("text-danger"),$(".flipLink").removeClass("hovCol"),$("#HeaderName").css("color","#343a40"),$("#GSAP").addClass("blueChangeGSAP").removeClass("redChange"),$(".fa-ul a, .subheading a").css("color","#389bbd"),$(".bg-danger").addClass("bg-info").removeClass("bg-danger"),$("#actionBtn").css("background-color","#389bbd"),$("h4 .text-muted").addClass("text-info").removeClass("text-muted"),$("h4 .text-form-dark").addClass("text-form").removeClass("text-form-dark"),$(".redChange").addClass("blueChange").removeClass("redChange"),$("i.devicons, i.newIcon, .fa-stack.fa-lg").removeClass("hovCol"),$(".img-profile").removeClass("Sith").addClass("Jedi"),$("#actionBtnBug").css("background-color","#ffc107"),$("#actionBtnChat").css("background-color","#20c997"),$(".modalAddon").removeClass("invertedBtn"),$(".modImg").removeClass("modImgSith"),$("#loading-wrapper").removeClass("inverted"),$(".secHeading").removeClass("subDark"),$(".chatSend").removeClass("btn-outline-danger").addClass("btn-outline-info"),$(".chatHead, .chatHolder, .chatInput").css("background-color","#fff"),localStorage.clear()})),"true"===localStorage.getItem("dark")&&e()}));
+function addSrc(e) {
+  document.getElementById(e).src = "https://" + e;
+}
+function addSrcImg(e) {
+  document.getElementById(e).src = e;
+}
+function playSaber(e) {
+  var a = new Audio(e);
+  a.play(), (a.volume = 0.3), localStorage.setItem("darkMode", "true");
+}
+-1 != navigator.userAgent.indexOf("Chrome") ||
+  -1 != navigator.userAgent.indexOf("Edg") ||
+  navigator.userAgent.indexOf("Firefox"),
+  "true" === sessionStorage.getItem("dismiss") &&
+    ((document.getElementById("infoAlert").style.display = "none"),
+    $("#infoAlert").alert("close")),
+  (window.onload = function () {
+    (document.getElementById("loading-container").style.display = "none"),
+      $(this).scrollTop() > 9e3 &&
+        $(".img-profile").attr("src", "img/FACE2.png"),
+      $(this).scrollTop() < 9e3 &&
+        $(".img-profile").attr("src", "img/profile.png");
+  }),
+  $(function () {
+    function e() {
+      $("html").addClass("dark_mode_on"),
+        $("#sideNav").removeClass("bg-primary").addClass("bg-dark"),
+        $("#darkMode").hide(),
+        $("#lightMode").show(),
+        $("body").addClass("dark_mode"),
+        $("#infoAlert").removeClass("inset").addClass("info_alert_dark"),
+        $(".list-group-flush .list-group-item").addClass("dark_mode3"),
+        $("li .btn").removeClass("btn-light").addClass("btn-dark"),
+        $(".badge-dark").addClass("badge-light").removeClass("badge-dark"),
+        $("h1,h2,h3").addClass("dark_mode2"),
+        $(".badge-info").addClass("badge-danger").removeClass("badge-info"),
+        $(".text-primary").addClass("text-danger").removeClass("text-primary"),
+        $(".flipLink").addClass("hovCol"),
+        $(".bg-info").addClass("bg-danger").removeClass("bg-info"),
+        $("h4 .text-info").addClass("text-danger").removeClass("text-info"),
+        $("h4 .text-form").addClass("text-form-dark").removeClass("text-form"),
+        $(".blueChange").addClass("redChange").removeClass("blueChange"),
+        $("#GSAP").addClass("redChange").removeClass("blueChangeGSAP"),
+        $("i.devicons, i.newIcon, .fa-stack.fa-lg").addClass("hovCol"),
+        $(".img-profile")
+          .addClass("avatar_dark_mode_modifier")
+          .removeClass("avatar_light_mode_modifier"),
+        $(".modalAddon").addClass("invertedBtn"),
+        $(".modImg").addClass("img_avatar_dark_mode_modifier"),
+        $("#loading-wrapper").addClass("inverted"),
+        $(".secHeading").addClass("subDark"),
+        $(".chatSend")
+          .removeClass("btn-outline-info")
+          .addClass("btn-outline-danger");
+    }
+    $(window).scroll(function () {
+      $(this).scrollTop() > 9e3 &&
+        $(".img-profile").attr("src", "img/FACE2.png"),
+        $(this).scrollTop() < 9e3 &&
+          $(".img-profile").attr("src", "img/profile.png");
+    }),
+      document.getElementById("darkMode").addEventListener("click", e),
+      document
+        .getElementById("lightMode")
+        .addEventListener("click", function () {
+          $("html").removeClass("dark_mode_on"),
+            $("#sideNav").removeClass("bg-dark").addClass("bg-primary"),
+            $("#lightMode").hide(),
+            $("#darkMode").show(),
+            $("body").removeClass("dark_mode"),
+            $("#infoAlert").removeClass("info_alert_dark").addClass("inset"),
+            $(".list-group-flush .list-group-item").removeClass("dark_mode3"),
+            $("li .btn").removeClass("btn-dark").addClass("btn-light"),
+            $(".badge-light").addClass("badge-dark").removeClass("badge-light"),
+            $("h1,h2,h3").removeClass("dark_mode2"),
+            $(".badge-danger")
+              .addClass("badge-info")
+              .removeClass("badge-danger"),
+            $(".text-danger")
+              .addClass("text-primary")
+              .removeClass("text-danger"),
+            $(".flipLink").removeClass("hovCol"),
+            $("#GSAP").addClass("blueChangeGSAP").removeClass("redChange"),
+            $(".bg-danger").addClass("bg-info").removeClass("bg-danger"),
+            $("h4 .text-muted").addClass("text-info").removeClass("text-muted"),
+            $("h4 .text-form-dark")
+              .addClass("text-form")
+              .removeClass("text-form-dark"),
+            $(".redChange").addClass("blueChange").removeClass("redChange"),
+            $("i.devicons, i.newIcon, .fa-stack.fa-lg").removeClass("hovCol"),
+            $(".img-profile")
+              .removeClass("avatar_dark_mode_modifier")
+              .addClass("avatar_light_mode_modifier"),
+            $(".modalAddon").removeClass("invertedBtn"),
+            $(".modImg").removeClass("img_avatar_dark_mode_modifier"),
+            $("#loading-wrapper").removeClass("inverted"),
+            $(".secHeading").removeClass("subDark"),
+            $(".chatSend")
+              .removeClass("btn-outline-danger")
+              .addClass("btn-outline-info"),
+            localStorage.clear();
+        }),
+      "true" === localStorage.getItem("darkMode") && e();
+  });
--- js/DarkMode.js
+++ js/DarkMode.js
@@ -1,3 +1,4 @@
+// Check user browser
 if (
   navigator.userAgent.indexOf("Chrome") != -1 ||
   navigator.userAgent.indexOf("Edg") != -1 ||
@@ -37,7 +38,7 @@ function playSaber(url) {
   var sound = new Audio(url);
   sound.play();
   sound.volume = 0.3;
-  localStorage.setItem("dark", "true");
+  localStorage.setItem("darkMode", "true");
 }
 // Photo change on scroll
 $(function () {
@@ -50,88 +51,78 @@ $(function () {
     }
   });
 
-  // Dark mode
-  document.getElementById("darkMode").addEventListener("click", dark);
-  function dark() {
+  // Dark/light mode listeners
+  document.getElementById("darkMode").addEventListener("click", darkModeSwitch);
+  document
+    .getElementById("lightMode")
+    .addEventListener("click", lightModeSwitch);
+  function darkModeSwitch() {
     $("html").addClass("dark_mode_on");
     $("#sideNav").removeClass("bg-primary").addClass("bg-dark");
     $("#darkMode").hide();
     $("#lightMode").show();
-    $("body").addClass("darkness");
-    $("#infoAlert").removeClass("inset").addClass("sabre");
-    $(".list-group-flush .list-group-item").addClass("darkness3");
-    $("#skillsAccordion").css("background-color", "#222");
+    $("body").addClass("dark_mode");
+    $("#infoAlert").removeClass("inset").addClass("info_alert_dark");
+    $(".list-group-flush .list-group-item").addClass("dark_mode3");
     $("li .btn").removeClass("btn-light").addClass("btn-dark");
     $(".badge-dark").addClass("badge-light").removeClass("badge-dark");
-    $("h1,h2,h3").addClass("darkness2");
+    $("h1,h2,h3").addClass("dark_mode2");
     $(".badge-info").addClass("badge-danger").removeClass("badge-info");
     $(".text-primary").addClass("text-danger").removeClass("text-primary");
     $(".flipLink").addClass("hovCol");
-    $("#HeaderName").css("color", "#ddd");
-    $(".fa-ul a, .subheading a").css("color", "#c20404");
     $(".bg-info").addClass("bg-danger").removeClass("bg-info");
-    $("#actionBtn").css("background-color", "#c20404");
     $("h4 .text-info").addClass("text-danger").removeClass("text-info");
     $("h4 .text-form").addClass("text-form-dark").removeClass("text-form");
     $(".blueChange").addClass("redChange").removeClass("blueChange");
     $("#GSAP").addClass("redChange").removeClass("blueChangeGSAP");
     $("i.devicons, i.newIcon, .fa-stack.fa-lg").addClass("hovCol");
-    $(".img-profile").addClass("Sith").removeClass("Jedi");
-    $("#actionBtnBug").css("background-color", "#ba8b00");
-    $("#actionBtnChat").css("background-color", "#fd7e14");
+    $(".img-profile")
+      .addClass("avatar_dark_mode_modifier")
+      .removeClass("avatar_light_mode_modifier");
     $(".modalAddon").addClass("invertedBtn");
-    $(".modImg").addClass("modImgSith");
+    $(".modImg").addClass("img_avatar_dark_mode_modifier");
     $("#loading-wrapper").addClass("inverted");
     $(".secHeading").addClass("subDark");
     $(".chatSend")
       .removeClass("btn-outline-info")
       .addClass("btn-outline-danger");
-    $(".chatHead, .chatHolder").css("background-color", "#343a40");
-    $(".chatInput").css("background-color", "#111");
   }
-  // Light mode
-  $("#lightMode").on("click", light);
 
-  function light() {
+  function lightModeSwitch() {
     $("html").removeClass("dark_mode_on");
     $("#sideNav").removeClass("bg-dark").addClass("bg-primary");
     $("#lightMode").hide();
     $("#darkMode").show();
-    $("body").removeClass("darkness");
-    $("#infoAlert").removeClass("sabre").addClass("inset");
-    $(".list-group-flush .list-group-item").removeClass("darkness3");
-    $("#skillsAccordion").css("background-color", "#fff");
+    $("body").removeClass("dark_mode");
+    $("#infoAlert").removeClass("info_alert_dark").addClass("inset");
+    $(".list-group-flush .list-group-item").removeClass("dark_mode3");
     $("li .btn").removeClass("btn-dark").addClass("btn-light");
     $(".badge-light").addClass("badge-dark").removeClass("badge-light");
-    $("h1,h2,h3").removeClass("darkness2");
+    $("h1,h2,h3").removeClass("dark_mode2");
     $(".badge-danger").addClass("badge-info").removeClass("badge-danger");
     $(".text-danger").addClass("text-primary").removeClass("text-danger");
     $(".flipLink").removeClass("hovCol");
-    $("#HeaderName").css("color", "#343a40");
     $("#GSAP").addClass("blueChangeGSAP").removeClass("redChange");
-    $(".fa-ul a, .subheading a").css("color", "#389bbd");
     $(".bg-danger").addClass("bg-info").removeClass("bg-danger");
-    $("#actionBtn").css("background-color", "#389bbd");
     $("h4 .text-muted").addClass("text-info").removeClass("text-muted");
     $("h4 .text-form-dark").addClass("text-form").removeClass("text-form-dark");
     $(".redChange").addClass("blueChange").removeClass("redChange");
     $("i.devicons, i.newIcon, .fa-stack.fa-lg").removeClass("hovCol");
-    $(".img-profile").removeClass("Sith").addClass("Jedi");
-    $("#actionBtnBug").css("background-color", "#ffc107");
-    $("#actionBtnChat").css("background-color", "#20c997");
+    $(".img-profile")
+      .removeClass("avatar_dark_mode_modifier")
+      .addClass("avatar_light_mode_modifier");
     $(".modalAddon").removeClass("invertedBtn");
-    $(".modImg").removeClass("modImgSith");
+    $(".modImg").removeClass("img_avatar_dark_mode_modifier");
     $("#loading-wrapper").removeClass("inverted");
     $(".secHeading").removeClass("subDark");
     $(".chatSend")
       .removeClass("btn-outline-danger")
       .addClass("btn-outline-info");
-    $(".chatHead, .chatHolder, .chatInput").css("background-color", "#fff");
     localStorage.clear();
   }
 
   // Setting DarkMode via localStorage
-  if (localStorage.getItem("dark") === "true") {
-    dark();
+  if (localStorage.getItem("darkMode") === "true") {
+    darkModeSwitch();
   }
 });
